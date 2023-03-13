1b4f4b9f08f16cce31d8f3c6d635a2642f60885f
S25Yull <choisy277@gmail.com>
2023-03-13 20:20:22 +0900

    자바의 정석 ~15강까지
    변수와 타입, 상수와 리터럴 등 추가

---
+++ ch01/src/Hello.java
@@ -0,0 +1,24 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ctrl + shift + L 단축키 전체 목록보기
+		//ctrl + + - 폰트 크기 증가 감소
+		//ctrl + D 한줄 삭제
+		//ctrl + alt + down 행단위 복사
+		//alt + shith + A 멀티컬럼 편집
+		//alt + up, down 행단위 이동
+		//ctrl + i 자동 들여쓰기
+		//ctrl + / 주석처리(토글)
+		//ctrl + space 자동완성
+		System.out.println("Hell1234o hh");
+		System.out.println("Hell1234o hh");
+		System.out.println("Hell1234o hh");
+		System.out.println("Hell1234o hh");
+		System.out.println("Hell1234o hh");
+		System.out.println("Hell1234o hh");
+		System.out.println("Hell1234o hh");
+	}
+
+}
---
+++ ch02/src/ch02/VarEx3.java
@@ -0,0 +1,25 @@
+package ch02;
+
+public class VarEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int score = 100;
+//		score = 200;
+		boolean power = true;
+		byte b = 127;
+		int oct = 010;
+		int hex = 0x10;
+		long l = 10_000_000_000L;
+		float f = 3.14f;
+		double d = 3.14f;
+				
+		char ch ='A';
+		int i = 'A';
+		String str2="";//빈 문자열도 가능
+		String str3="123";
+		String str4=str2+str3;
+		System.out.println(i);
+	}
+
+}
