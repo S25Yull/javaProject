d5bc941de660af3c896e83de23456995aa6e3828
S25Yull <choisy277@gmail.com>
2023-03-15 17:59:44 +0900

    자바의 정석~39

---
+++ ch04/src/ch04/Ex4_17.java
@@ -0,0 +1,14 @@
+package ch04;
+
+public class Ex4_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=10;i++) {
+			if(i%2==0)
+				continue;//2의 배수면 pass
+			System.out.println(i);
+		}
+	}
+
+}
